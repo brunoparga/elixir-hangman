03c6e4a16b1f3bf3db0a9358948c61bbcc9f7595
Bruno Parga <brunoparga@gmail.com>
2018-11-12 22:41:18 -0300

    Make Hangman into a server application

--- test/hangman_test.exs
+++ test/hangman_test.exs
@@ -4,8 +4,7 @@ defmodule HangmanTest do
 
   test "a single letter is an accepted move" do
     new_game = Hangman.new_game()
-    {game, tally} = Hangman.make_move(new_game, "x")
-    assert game.game_state == :bad_guess
-    assert tally.game_state == game.game_state
+    tally = Hangman.make_move(new_game, "x")
+    assert tally.game_state == :bad_guess
   end
 end
