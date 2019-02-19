4dff8c1b14dfe5c5baa7eee1dae7efa5533c16fb
Bruno Parga <brunoparga@gmail.com>
2019-02-18 23:53:17 -0300

    Set up new client interface

--- text_client/lib/text_client/interact.ex
+++ text_client/lib/text_client/interact.ex
@@ -1,10 +1,23 @@
 defmodule TextClient.Interact do
+  @hangman_server :"hangman@Librem-15"
+
   alias TextClient.{Player, State}
 
   def start() do
-    Hangman.new_game()
+    new_game()
     |> setup_state()
     |> Player.play()
+  end
+
+  defp new_game() do
+    Node.connect(@hangman_server)
+
+    :rpc.call(
+      @hangman_server,
+      Hangman,
+      :new_game,
+      []
+    )
   end
 
   defp setup_state({pid, tally}) do
